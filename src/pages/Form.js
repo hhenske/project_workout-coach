07c62352db5665102f8feced280a36c87a080168
Holly Henske <hcover333@gmail.com>
2021-10-09 14:30:41 -0500

    added handlers in form, but am having an error I don't know how to fix

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import classes from './form.module.css';
-
+import SuggestedWorkout from './SuggestedWorkout';
 
 const Form = () => {
 
+  const categoryChoiceHandler = (event) => {
+    const [enteredCategory, setEnteredCategory] = useState('');
+    setEnteredCategory(event.target.value);
+    
+  };
+
+  const intensityChoiceHandler = (event) => {
+    const [enteredIntensity, setEnteredIntensity] = useState('');
+    setEnteredIntensity(event.target.value);
+    
+  };
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+  };
+
 return (
-  
-    <form>
+  <Fragment>
+    <form onSubmit={formSubmissionHandler}>
       <div className="text-center">
         <br /><br />
         <h2>Let's choose a workout!</h2>
         <br />
         <label><h3>What kind of workout would you like today?</h3>
-          <select name = "workoutCategory">
+          <select name = "workoutCategory" onChange={categoryChoiceHandler}>
                         <option value = "">* Choose an activity *</option>
                         <option value="1">Core Exercises</option>
                         <option value="2">Strength Training</option>
@@ -23,16 +39,21 @@ return (
         <br />
         <div className="text-center">
         <label><h4>How are you feeling today?</h4>
-          <select name = "workoutIntensity">
+          <select name = "workoutIntensity" onChange={intensityChoiceHandler}>
                         <option value = "">* Choose one*</option>
                         <option value="1">Great! up for a super hard workout!</option>
                         <option value="2">Fine! Ready to work out!</option>
                         <option value="3">Still tired or sore from last workout</option>
-                        <option value="4">fatigued or sick</option>
+                        <option value="4">Extra-fatigued or sick</option>
             </select></label>
            
           </div>
         </form>
+        <div>
+          <SuggestedWorkout />
+        </div>
+  </Fragment>
+        
       
 )};
 
---
+++ src/pages/SuggestedWorkout.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+const SuggestedWorkout = () => {
+
+    return (
+        <Fragment>
+            <h2>How about....</h2>
+            <div>Run / Walk intervals: 5 min / 2 min</div>
+        </Fragment>
+
+    );
+};
+
+export default SuggestedWorkout;
